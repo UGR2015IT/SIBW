141b12ac13e84d73ccabb0522066e281d68b5b1f
dave95 <davidegallitelli@gmail.com>
2016-03-15 18:48:05 +0100

    Working on P1, updated to three-col structure

--- Practica/Practica 1/Hotel Plaza Nueva/assets/js/new.js
+++ Practica/Practica 1/Hotel Plaza Nueva/assets/js/new.js
@@ -1,3 +1,7 @@
-function myFoo(){
+function myFoo() {
     alert("Function not implemented yet. Sorry for the trouble.");
+}
+
+function changeZoomPicture(newPic) {
+    document.getElementById("zoom-pic").src = "images/fotos/" + newPic;
 }
