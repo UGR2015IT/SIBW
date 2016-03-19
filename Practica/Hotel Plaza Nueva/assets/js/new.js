3b32d3b50f970cf696262ffa56c3059b2a4e1cfd
dave95 <davidegallitelli@gmail.com>
2016-03-19 21:42:18 +0100

    Completed P1

--- Practica/Hotel Plaza Nueva/assets/js/new.js
+++ Practica/Hotel Plaza Nueva/assets/js/new.js
@@ -7,7 +7,7 @@ function changeZoomPicture(newPic) {
 }
 
 function highlightBG(paramId) {
-    document.getElementById(paramId).css.background = "#000";
+    document.getElementById(paramId).style.background = "#000";
 }
 
 function today() {
@@ -17,13 +17,34 @@ function today() {
     var yyyy = day.getFullYear();
 
     if(dd<10) {
-        dd='0'+dd
+        dd='0'+dd;
     } 
 
     if(mm < 10) {
-        mm='0'+mm
+        mm='0'+mm;
     } 
 
     day = yyyy+'-'+mm+'-'+dd;
     document.getElementById("input-llegada").min=day;
+}
+
+function showPrice(paramId) {
+    var dom = document.getElementById(paramId);
+    if (dom.style.display == "inline")
+        dom.style.display = "none";
+    else dom.style.display = "inline";
+}
+
+function updateReserva(paramId2){
+    var select = document.getElementById('habitacion-select');
+    var option, i=0;
+
+    for (i=0; i<select.options.length; i++) {
+        option = select.options[i];
+
+        if (option.id == paramId2) {
+         option.selected = true;
+         return;
+      } 
+    }
 }
