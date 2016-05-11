bf67d58fff4e653e8fb7d61f67130b8523c68df6
Davide Gallitelli <davidegallitelli@gmail.com>
2016-05-11 19:35:29 +0200

    HTML Frame for admin page

--- Practica/Hotel Plaza Nueva/include/content/functions/admin.php
+++ Practica/Hotel Plaza Nueva/include/content/functions/admin.php
@@ -1,5 +1,39 @@
-HOLA ADMIN
-
-<?php
+<section id="admin" class="two">
+        <h1>Bienvenido, <?php echo $_SESSION['user'] ?></h1>
+        <h4>Gestion Hotel</h4>
+        <ul>
+            <li>Habitaciones</li>
+            <ul>
+                <li>Quadrante stanze</li>
+                <li>Inserisci nuove stanze</li>
+            </ul>
+            <li>Promociones</li>
+            <ul>
+                <li>Visualizza/Modifica</li>
+                <li>Aggiungi</li>
+                <li>Elimina</li>
+            </ul>
+            <li>Actividades</li>
+        </ul>
+        <br />
+        <h4>Reservas</h4>
+        <ul>
+            <li>Visualizar</li>
+            <li>Insertar</li>
+            <li>Modificar</li>
+            <li>Borrar</li>
+        </ul>
+        <br />
+        <h4>Usuarios</h4>
+        <ul>
+            <li>Visualizar</li>
+            <li>Insertar</li>
+            <li>Modificar</li>
+            <li>Borrar</li>
+        </ul>
+        
+        <?php
     include 'include/content/modules/logout.php';
 ?>
+        
+</section>
